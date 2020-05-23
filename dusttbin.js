4d148dce1a2ae92654b0f9523a1fcf0d08a1d6ce
Parth Singh <65109271+Parth-AI@users.noreply.github.com>
2020-05-23 09:31:07 +0530

    Add files via upload

---
+++ dusttbin.js
@@ -0,0 +1,24 @@
+class Dustbin extends ParentClass{
+     constructor(x, y, width, height) {
+         /* var options = {
+              'restitution':0.8,
+              'friction':100.0,
+              //'density':10.0
+          }
+          this.body = Bodies.rectangle(x, y, width,height,options);
+          this.width = width;
+          this.height = height;
+      
+          World.add(world, this.body);*/
+          super(x,y,width,height);
+        }
+       /* display(){
+         /* var pos =this.body.position;
+          push();
+          translate(pos.x,pos.y);
+          rectMode(CENTER);
+          fill(255);
+          rect(0,0, this.width, this.height);
+          pop();*/
+        //}
+}
